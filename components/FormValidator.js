ee2234312e4075cb22eff7147f25ba21f73ffed0
Missy <mhyatt91@yahoo.com>
2026-01-20 14:09:15 -0500

    finished project

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,5 +1,6 @@
 class FormValidator {
   constructor(settings, formEl) {
+    this._settings = settings;
     this._inputSelector = settings._inputSelector;
     this._submitButtonSelector = settings._submitButtonSelector;
     this._errorClass = settings._errorClass;
@@ -10,36 +11,44 @@ class FormValidator {
 
   _checkInputValidity(inputElement) {
     if (!inputElement.validity.valid) {
-      showInputError(
-        formElement,
-        inputElement,
-        inputElement.validationMessage,
-        settings
-      );
+      this._showInputError(inputElement, inputElement.validationMessage);
     } else {
-      hideInputError(formElement, inputElement, settings);
+      this._hideInputError(inputElement);
     }
-    //work through errors in console
   }
 
   setEventListeners() {
     this._inputList = Array.from(
-      this.formEl.querySelectorAll(this._inputSelector)
+      this._formEl.querySelectorAll(this._settings.inputSelector)
     );
 
-    // finish implementing _setEventListeners
-    const buttonElement = formElement.querySelector(
-      settings.submitButtonSelector
+    const buttonElement = this._formEl.querySelector(
+      this._settings.submitButtonSelector
     );
 
-    toggleButtonState(inputList, buttonElement, settings);
+    this._toggleButtonState(this._inputList, buttonElement, this._settings);
 
     this._inputList.forEach((inputElement) => {
       inputElement.addEventListener("input", () => {
+        console.log(1);
         this._checkInputValidity(inputElement);
-        toggleButtonState(inputList, buttonElement, settings);
+        this._toggleButtonState(this._inputList, buttonElement, this._settings);
       });
     });
+  }
+
+  _toggleButtonState() {
+    // this function receives the inputsList, therefore we need to go through the list and check are all inputs valid.
+    // if yes - button is enabled, otherwise - not
+    if (
+      this._inputList.every((item) => {
+        return item.valid;
+      })
+    ) {
+      this._buttonElement.disabled = false;
+    } else {
+      this._buttonElement.disabled = true;
+    }
   }
 
   enableValidation() {
--- components/Todo.js
+++ components/Todo.js
@@ -5,18 +5,16 @@ class Todo {
   }
 
   _setEventListeners() {
-    todoCheckboxEl.addEventListener("change", () => {
+    this._todoCheckboxEl.addEventListener("change", () => {
       this._data.completed = !this._data.completed;
-      console.log(this._data.completed);
       //check to see if true and false show in console
     });
   }
 
   _generateCheckboxEl() {
-    this._todoCheckboxEl = this._todoElement =
-      this._todoElement.querySelector(".todo__completed");
-    this._todoLabel = this._todoElement =
-      this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+
     this._todoCheckboxEl.checked = this._data.completed;
     this._todoCheckboxEl.id = `todo-${this._data.id}`;
     this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
@@ -34,7 +32,7 @@ class Todo {
 
     todoNameEl.textContent = this._data.name;
 
-    this._generateCheckbox();
+    this._generateCheckboxEl();
     this._setEventListeners();
 
     return this._todoElement;
